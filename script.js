93fd4e432efec465f0292d904a3d73722aebb738
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2023-10-25 05:15:12 -0400

    got functioning grid, have to add onHover effects.

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 // 16x16 divs...
-const container = document.querySelector(".grid-container");
-for(let i=0;i<16;i++) // initialize row divs?
+//const rowContainer = document.querySelector(".row-container");
+//const columnContainer=document.querySelector(".column-container");
+/*const body = document.querySelector("body");
+const mainRowWrapper=document.querySelector(".main-row-wrapper");
+const mainColWrapper=document.querySelector(".main-col-wrapper");
+
+for(let q=0;q<16;q++){
+    const rowContainer=document.createElement('div');
+    rowContainer.setAttribute("class","rowContainer"+q);
+    mainRowWrapper.appendChild(rowContainer);
+    const rowDivs=document.createElement('div')
+    rowDivs.setAttribute("class","x"); // container
+    let x=document.createElement('div');
+    x.textContent=("0");
+    x.setAttribute("class","row");
+    rowDivs.appendChild(x);
+    rowContainer.appendChild(rowDivs);
+}*/
+const flexGrid = document.querySelector(".flex-grids");
+
+for(let i=0;i<256;i++)
 {
-    x=document.createElement('div');
+    let col=document.createElement('div');
+    col.setAttribute("class","column");
+    flexGrid.appendChild(col);
 }
