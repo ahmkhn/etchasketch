df61d44214e0f3b94246e2e7b17bc3a7b3243d4c
Ahmed Khan <104601942+ahmkhn@users.noreply.github.com>
2023-10-25 05:41:21 -0400

    added on hover + clear button. may revisit this in the future to add more features.

--- script.js
+++ script.js
@@ -1,22 +1,5 @@
 // 16x16 divs...
-//const rowContainer = document.querySelector(".row-container");
-//const columnContainer=document.querySelector(".column-container");
-/*const body = document.querySelector("body");
-const mainRowWrapper=document.querySelector(".main-row-wrapper");
-const mainColWrapper=document.querySelector(".main-col-wrapper");
 
-for(let q=0;q<16;q++){
-    const rowContainer=document.createElement('div');
-    rowContainer.setAttribute("class","rowContainer"+q);
-    mainRowWrapper.appendChild(rowContainer);
-    const rowDivs=document.createElement('div')
-    rowDivs.setAttribute("class","x"); // container
-    let x=document.createElement('div');
-    x.textContent=("0");
-    x.setAttribute("class","row");
-    rowDivs.appendChild(x);
-    rowContainer.appendChild(rowDivs);
-}*/
 const flexGrid = document.querySelector(".flex-grids");
 
 for(let i=0;i<256;i++)
@@ -25,3 +8,16 @@ for(let i=0;i<256;i++)
     col.setAttribute("class","column");
     flexGrid.appendChild(col);
 }
+const columns = document.querySelectorAll('.column');
+
+columns.forEach(columns =>{
+    columns.addEventListener('mouseenter',function(){
+        columns.style.backgroundColor="red";
+    })
+});
+function clearClicked()
+{
+    columns.forEach(columns=>{
+        columns.style.backgroundColor="white";
+    })
+};
